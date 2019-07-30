ee5ebadcccc3426743cf5663997c5107e5790633
jinusong <sugwer7464@gmail.com>
2019-07-30 10:13:13 +0900

    [Add] Project

---
+++ src/test/kotlin/com/jinwoo/todoList/TodoListApplicationTests.kt
@@ -0,0 +1,16 @@
+package com.jinwoo.todoList
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class TodoListApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
